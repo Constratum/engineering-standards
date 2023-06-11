850e41412f280db7119eabab6332d92772389b4c
Matt Bishop <129570940+MattBishop81@users.noreply.github.com>
2023-06-12 11:19:28 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,8 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="Constratum_engineering_standards",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=["numpy", "pandas"],
+)
