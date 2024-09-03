0514b9c27a58d882db78e542c316e1435f7144ab
Matt Bishop <129570940+MattBishop81@users.noreply.github.com>
2024-09-03 15:04:36 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="engineering_standards",
-    version="0.0183",
+    version="0.0184",
     packages=find_packages(),
     install_requires=["numpy", "pandas"],
 )
