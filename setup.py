4a2b38fa03c539d451cd368fd013d05d71ccd94e
nimash1993 <115510557+nimash1993@users.noreply.github.com>
2024-10-15 20:19:21 +1300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="engineering_standards",
-    version="0.0197",
+    version="0.0198",
     packages=find_packages(),
     install_requires=["numpy", "pandas"],
 )
