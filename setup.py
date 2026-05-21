de341b3c5df71d7a76f873d649fe61a05b98d564
nimash1993 <115510557+nimash1993@users.noreply.github.com>
2026-05-22 11:45:49 +1200

    Bump version from 0.12 to 0.13
    Add Aluminium Out of Box alloy type

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="engineering_standards",
-    version="0.12",
+    version="0.13",
     packages=find_packages(),
     install_requires=["numpy", "pandas"],
 )
