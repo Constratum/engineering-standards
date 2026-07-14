c6988ef1218415b1172f837d0dfc33777f83f6b1
nimash1993 <115510557+nimash1993@users.noreply.github.com>
2026-07-14 23:10:07 +1200

    Bump version from 0.18 to 0.19

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="engineering_standards",
-    version="0.18",
+    version="0.19",
     packages=find_packages(),
     install_requires=["numpy", "pandas"],
 )
