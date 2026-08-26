f887b1efd087b643538b0b6bb8fe0eec23507ac3
nimash1993 <115510557+nimash1993@users.noreply.github.com>
2026-08-26 20:29:38 +1200

    adding AS4084-2023 for AS-Standards

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="engineering_standards",
-    version="0.22",
+    version="0.23",
     packages=find_packages(),
     install_requires=["numpy", "pandas"],
 )
