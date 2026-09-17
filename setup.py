70637292d0f4129c2dd4e853d344a2a2b0e13d13
nimash1993 <115510557+nimash1993@users.noreply.github.com>
2026-09-18 00:18:49 +1200

    racking out-of=plumb update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="engineering_standards",
-    version="0.26",
+    version="0.27",
     packages=find_packages(),
     install_requires=["numpy", "pandas"],
 )
